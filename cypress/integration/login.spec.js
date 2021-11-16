51658174ab4bb1427b0f9db884ece11656f7c178
Cristhian Arispe Teran <Cristhian.Arispe@jalasoft.com>
2021-11-16 16:11:25 -0400

    latest changes on main version

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,11 +1,7 @@
 
 describe('Login Page', () => {
 
-    it('Verify it is possible loggin in', () => {
-        cy.login();
-                
-    });
-    
+    /*    
     it('Verify a new task can be added in To Do', () => {
         cy.login();
         cy.contains('div.cu-data-view-item__name-text', 'Board').click();
@@ -37,16 +33,14 @@ describe('Login Page', () => {
         cy.get('div.cu-panel-board__enter-button').click();
         cy.wait(3000);
         cy.contains('This is the test task in NEEDS REVIEW1.1').should('be.visible');
-    });
+    });*/
 
     it('Verify a task can be deleted from the Board', () => {
         cy.login();
         cy.contains('div.cu-data-view-item__name-text', 'Board').click();
         cy.wait(5000);
         cy.contains('section[class^="cdk-drag cu-panel-board cu-panel-board__task"][class$="ng-trigger ng-trigger-loadingEnter ng-star-inserted"]', 'Deleted task').rightclick();
-        cy.wait(3000);
         cy.get('a[class="nav-menu-item nav-menu-item__top-choices-option ng-star-inserted"]').eq(4).click();
-        cy.wait(3000);
         cy.contains('section[class^="cdk-drag cu-panel-board cu-panel-board__task"][class$="ng-trigger ng-trigger-loadingEnter ng-star-inserted"]', 'Deleted task').should('not.exist');
     });
 
@@ -55,7 +49,6 @@ describe('Login Page', () => {
         cy.contains('div.cu-data-view-item__name-text', 'Board').click();
         cy.wait(5000);
         cy.contains('section[class^="cdk-drag cu-panel-board cu-panel-board__task"][class$="ng-trigger ng-trigger-loadingEnter ng-star-inserted"]', 'Edited task').click();
-        cy.wait(5000);
         cy.get('textarea[class^="cdk-textarea-autosize task-name"][class$="ng-star-inserted"]').dblclick().clear();
         cy.get('textarea[class^="cdk-textarea-autosize task-name"][class$="ng-star-inserted"]').dblclick().type('This task was EDITED');
         cy.get('div[tooltipmodifier="tooltip_left-nowrap"]').click();
